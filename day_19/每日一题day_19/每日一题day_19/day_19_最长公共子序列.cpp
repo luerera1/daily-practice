035ecb8fafc161a25243a41b46f7f86e147c9970
luerera1 <luerera@163.com>
2019-11-26 22:35:29 +0800

    最长公共子序列

---
+++ day_19/每日一题day_19/每日一题day_19/day_19_最长公共子序列.cpp
@@ -0,0 +1,29 @@
+链接：https://www.nowcoder.com/questionTerminal/181a1a71c7574266ad07f9739f791506
+来源：牛客网
+
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+	string a, b;
+	while (cin >> a >> b)
+	{
+		if (a.size() > b.size())
+			swap(a, b);
+		string str_m;//存储最长公共子串
+		for (int i = 0; i < a.size(); i++)
+		{
+			for (int j = i; j < a.size(); j++)
+			{
+				string temp = a.substr(i, j - i + 1);
+				if (int(b.find(temp))<0)
+					break;
+				else if (str_m.size() < temp.size())
+					str_m = temp;
+			}
+		}
+		cout << str_m << endl;
+	}
+	return 0;
+}
